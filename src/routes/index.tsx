8594a38d2782707d0bf1422f64979a58bcad4fa1
Ruiz Asis Gustavo Enrique <gustavo.ruiz@supervielle.com.ar>
2023-02-08 20:46:48 -0300

    papi2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,16 +4,7 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hola luisi</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <Title>El Barba</Title>
     </main>
   );
 }
