facfbbaa7488cb49cea404ad429c8c1df5787a79
Luisi <arsori82@gmail.com>
2023-02-08 20:34:38 -0300

    luisi3

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,13 +6,7 @@ export default function Home() {
     <main>
       <Title>Barba</Title>
       <h1>El barba</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-      </p>
+      <img src="MENU.jpg" >
     </main>
   );
 }
